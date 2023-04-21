9ec486b5e0d8000f0d73ba0f22d02a041a7aac4e
jflohr3 <jflohr3@gmail.com>
2023-04-21 01:02:16 +0000

    Today's exercises, and trying out this 'free' online compiler

---
+++ Modern C++ Tutorial: C++11/14/17/20 On the Fly/exercise1.cpp
@@ -0,0 +1,4 @@
+int main()
+{
+  //TODO redo the binding ex until grok
+}
---
+++ Modern C++ Tutorial: C++11/14/17/20 On the Fly/exercise2.cpp
@@ -0,0 +1,65 @@
+//TODO alter Mean such that no manual state reset is necessary
+//     check type safety and provide assurance for any int width
+
+//TODO find a coding standard YOU prefer. List reasons. Alter code to adhere.
+//TODO Read next chapter of "Destination Unknown", you lemon
+#include <iostream>
+using namespace std;
+
+class Mean
+{
+public:
+  template<typename T>
+  void proc(T num)
+  {
+    ++numNums;
+    runningSum += num;
+    mean = (double)runningSum/numNums;
+  }
+ 
+  template<typename T, typename... Ts>
+  void proc(T num, Ts... nums)
+  {
+    ++numNums;
+    runningSum += num;
+    proc(nums...);
+  }
+  
+  void print(void)
+  {
+    cout << "runningSum: " << runningSum << endl;  
+    cout << "numNums: " << numNums << endl;  
+    cout << "mean: " << mean << endl;  
+  }
+  void reset(void)
+  {
+    runningSum =0;  
+    numNums =0;  
+    mean =0;  
+  }
+private:
+  int runningSum = 0;
+  int numNums = 0;
+  double mean = 0.0;
+  
+};
+
+
+// TODO allow invocation with a number list, with safety checks.
+//    Prove you didn't need to read that one K&R chapter.
+int main()
+{
+  Mean myClass{}; 
+  myClass.print();
+  
+  myClass.proc(1,2,3,4,5,6,7,8,9,10);
+  myClass.print();
+  myClass.reset();
+  
+  
+  myClass.proc(1l,2.0L,4,4,5ll,6,7ull,8,9ul,10.0f);
+  myClass.print();
+  myClass.reset();
+  
+  return(EXIT_SUCCESS);
+}
