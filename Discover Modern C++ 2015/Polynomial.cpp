ddc809ea1d3254b31b347981fa80528cfd2552a8
jflohr3 <jflohr3@gmail.com>
2023-06-14 15:21:25 -0600

    Polynomial.cpp can represent real polynomials with param pack constr, protected data, and print
    Use folds to gracefully unpack param pack, syntax is odd.
    
    printing real polynomial alterations for readability
    
    don't forget to reexamine folds due to recent usage

--- Discover Modern C++ 2015/Polynomial.cpp
+++ Discover Modern C++ 2015/Polynomial.cpp
@@ -1,13 +1,15 @@
 /* Problem Statement:
  *  Implement a class to handle representation of a polynomial
+ *  Support Real number coefficients, but not complex polynomials.
+ *    Because rigor..
  */
-
- /*! Support Real number coefficients, but not complex polynomials.
-  *    Because rigor..
-  */
   
-// TODO issue with param packed structs passed to push_back in Constructor
- 
+// DONE issue with param packed structs passed to push_back in Constructor.
+//        Use folds to expand the param pack. We were passing multiple structs
+//        objects to push_back(a,b,c) like a noob
+// !TODO push_back is copying the stack located structs. leave for now be aware
+// TODO peruse details of folds due to their powerful ability
+
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -32,11 +34,12 @@ class Cpolynomial
     cout << "Print Polynomial\n";
     
     int exp = 0;
-    for( Cfloat& v:coef_list ) // Ranges are nice thanks c++20
+    for( Cfloat& v:coef_list ) // Ranges are nice thanks c++20, too bad don't need here
     {
       if(exp != 0)
       {
-        cout << v.mantissa * pow(10, v.exponent);
+        cout << " +";
+        cout << " " << v.mantissa * pow(10, v.exponent);
         cout << "x^";
         cout << exp++;
       }
@@ -54,7 +57,14 @@ class Cpolynomial
     cout << "Constructor\n";
     cout << "Must be in order of LSP to MSP, starting with constant term\n";
     cout << "  include zero coefficients\n";
-    coef_list.push_back(these...); 
+    
+    /* FOLD expression to itr the param pack
+     *   The syntax is quite gnarly "([&] {loop-code} (), ...)"
+     */
+    ([&]
+    {
+      coef_list.push_back(these);
+    }(), ...);
   }
   
   private:
@@ -65,10 +75,10 @@ int main()
 {
   // Least squares approx of unit circle
   Cfloat a = {( (12*(pow(M_PI,2)-10))/ pow(M_PI,3)),0};
-  Cfloat b = {( (60*(pow(M_PI,2)-12))/ pow(M_PI,4)),0};
+  Cfloat b = {( (-60*(pow(M_PI,2)-12))/ pow(M_PI,4)),0};
   Cfloat c = {( (60*(pow(M_PI,2)-12))/ pow(M_PI,5)),0};
 
-  Cpolynomial MyPolynomial(a);
+  Cpolynomial MyPolynomial(a,b,c);
   
   MyPolynomial.print();
 
