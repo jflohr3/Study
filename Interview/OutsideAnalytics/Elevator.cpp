9a57e1307990fb5773887dabb1073f12b655c466
jflohr3 <jflohr3@gmail.com>
2023-06-20 12:58:43 -0600

    Update Elevator.cpp
    online gdb timed out again... fun
    
    currently figuring out how stl wants us to temp iterate an interator, apparently it supports []

--- Interview/OutsideAnalytics/Elevator.cpp
+++ Interview/OutsideAnalytics/Elevator.cpp
@@ -9,14 +9,14 @@
 • Inputs: [list of floors to visit] (e.g elevator start=12 floor=2,9,1,32)
 • Outputs: [total travel time, floors visited in order] (e.g 560 12,2,9,1,32)
 • Program Constants: Single floor travel time: 10
-
 *******************************************************************************/
 #include <cstdlib>
 #include <iostream>
+using namespace std;
 
 #include <regex>
 
-using namespace std;
+  //TODO only use endl 
 
 int main(int argc, char* argv[])
 {
@@ -26,36 +26,76 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE; 
   }
   
-  // TODO Move this printout to integer check regex
-  printf("================================================================\n");
-  printf("you handed me arguments:\n");
-  for(int i=1; i<argc;++i)
-    printf("arg %d:%s\n", i, argv[i]);
-  printf("================================================================\n");
+  /* Input numerical constraints
+   * Most floors in a building on earth - 154
+   * We include negative floors, basements in our solutions
+   * Therefore input type must be int16_t
+   */
+  cout << "Integer inputs must fit inside int_16t" << endl;
+  cout << "[2^15-1, 2^15] or [-32,767 , 32,768], inclusive" << endl;
   
-  std::regex int_regex("^-?[0-9]+");
+  /* Operation numerical constraints
+   * Largest floor difference possible given inputs is 2^16
+   * This value will be multiplied by 10, so difference container needs types
+   *   holding 2^16*10, therefore we use int32_t
+   * There is no limit to number of stops so we want the largest unsigned
+   *   type to hold the running sum of total travel time uint64_t.
+   */
+  //TODO check for overflows during running sum
+  
   /* Param check
    * Expect only inputs from the integer set
    *
    * Sol: Create a regex for the integer number set,
    *  check each input string, fail on any non-integer
    */
+  std::regex int_regex("^-?[0-9]+");
+  cout << "Input Arguments:\n";
+  cout << "================================================================";
+  cout << endl;
   for(int i=1; i<argc; ++i)
   {
-    if(!(std::regex_match(argv[i], int_regex)))
+    if( !(std::regex_match(argv[i], int_regex)) )
     {
       cout << "I only accept integer values with pos or neg prefix\n";
       cout << "\"" << argv[i] << "\"" << " is not an integer\n";
+      cout << "Exiting...\n";
+      exit(EXIT_FAILURE);
+    }
+    else
+    {
+      cout << "Arg " << i-1 << ": " << argv[i] << endl;
     }
   }
-   
-   
+  cout << "================================================================";
+  cout << endl;
+  
+  /*
+  vector<int16_t> floor_list;
+  vector<int32_t> travel_time_list;
+  uint64_t travel_time_sum = 0;
+  *
+  * We can do the floor_list and travel_time_list in the same container 
+  *   to save space.
+  */
+  vector<int32_t> floor_list;
+  vector<int32_t>::iterator floor_list_itr;
+  
+  // Populate floor list with inputs
+  for(int i=1; i<argc; ++i)
+  {
+    floor_list.push_back( strtol(argv[i], NULL, 10) );
+  }
+  
+  floor_list_itr = floor_list.begin();
+  while(floor_list_itr != floor_list.end())
+  {
+    // This = (this)
+    *floor_list_itr = abs(*floor_list_itr - *(floor_list_itr[1])) * 10;
+  }
   
   
   
   
-  
-  
-    cout<<"Hello World";
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
